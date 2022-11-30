1fa9093386c4395787e587132b153cf7cd9b0933
unknown <affanashfaq786@gmail.com>
2022-11-30 09:05:31 -0800

    Work Accomplish

---
+++ app.js
@@ -0,0 +1,14 @@
+var abc = "Welcome To This Page"
+alert(abc)
+var inp = document.getElementById("gnp")
+
+function addval(a){
+    gnp.value += a;
+}
+function cleargnp(){
+    gnp.value = "";
+}
+function equalgnp(){
+    var a = eval(gnp.value);
+    gnp.value = a;
+}
